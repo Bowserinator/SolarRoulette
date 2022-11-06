bff74a36f8844def60b5cc1792cdc34fa8000785
Bowserinator <bowserinator@gmail.com>
2022-11-05 21:36:40 -0400

    Add fancy 3D render

--- js/draw.js
+++ js/draw.js
@@ -10,22 +10,30 @@ const CELESTIAL_SPHERE = 1200; // Radius of stars
 const DRAW_SPHERE = 1000; // Radius of plane to draw stuff
 const AMBIENT_LIGHT_INTENSITY = 0.1;
 
+const container = document.getElementById('canvas-container');
+
 // WebGL1 renderer required for certain shader options
 const renderer = new THREE.WebGL1Renderer({
 	powerPreference: 'high-performance',
 	antialias: true
 });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+let base = getBase();
+renderer.setSize(base.innerWidth, base.innerHeight);
+document.getElementById('canvas-container').appendChild(renderer.domElement);
+renderer.domElement.classList.add('three-canvas');
+renderer.domElement.id = 'sky-canvas';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('black');
 
-const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 100000);
+const camera = new THREE.PerspectiveCamera(40, base.innerWidth / base.innerHeight, 1, 100000);
 const controls = new OrbitControls( camera, renderer.domElement );
 
 controls.minDistance = 1;
 controls.maxDistance = 100;
+
+controls.minAzimuthAngle = Math.PI - Math.PI / 4; // radians
+controls.maxAzimuthAngle = Math.PI + Math.PI / 4; // radians
 
 camera.position.set( 0, 0, 100 );
 controls.update();
@@ -33,7 +41,7 @@ controls.update();
 // Bloom + effects
 const composer = new EffectComposer(renderer);
 composer.addPass(new RenderPass(scene, camera));
-const bloomPass = new UnrealBloomPass(new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85);
+const bloomPass = new UnrealBloomPass(new THREE.Vector2( base.innerWidth, base.innerHeight ), 1.5, 0.4, 0.85);
 bloomPass.threshold = 0;
 bloomPass.strength = 0.25;
 bloomPass.radius = 0;
@@ -45,6 +53,9 @@ composer.addPass(afterImagePass);
 // Add ambient light for the ground
 const ambientLight = new THREE.AmbientLight(0xabcbff, AMBIENT_LIGHT_INTENSITY);
 scene.add(ambientLight);
+
+let isSun = false;
+let groundPlane;
 
 // Ground plane
 (() => {
@@ -65,6 +76,7 @@ scene.add(ambientLight);
     mesh.geometry.center();
     mesh.position.y = -100.0;
     mesh.rotation.x = -Math.PI / 2;
+    groundPlane = mesh;
 
     scene.add(mesh);
 
@@ -154,8 +166,9 @@ function createPlanet(size, img, override=[-1, -1]) {
     objMaterial.side = THREE.BackSide;
 
     const planetMesh = new THREE.Mesh(objGeometry, objMaterial);
-    planetMesh.rotation.y = ([{ size: 0 }].concat(planets)).map(p => p.size + 0.1).reduce((a, b) => a + b);
+    // planetMesh.rotation.y = ([{ size: 0 }].concat(planets)).map(p => p.size + 0.1).reduce((a, b) => a + b);
     planetMesh.position.y = 300 - height * 0.2;
+    console.log(planetMesh.position.y)
     scene.add(planetMesh);
 
     planets.push({
@@ -167,20 +180,61 @@ function createPlanet(size, img, override=[-1, -1]) {
         Math.min(0.5, planets.map(p => p.size ** 2).reduce((a, b) => a + b));
 }
 
-createPlanet(2439, 'mercury.png');
-createPlanet(6051.8, 'venus.png');
-createPlanet(6371, 'earth.png');
-createPlanet(1737.4, 'moon.png');
-createPlanet(3389.5, 'mars.png');
+window.onChangeBody = body => {
+    planets.forEach(p => scene.remove(p.mesh));
+    planets = [];
+    isSun = false;
+    skyMesh.visible = true;
+    groundPlane.visible = true;
 
-createPlanet(69911, 'jupiter.png', [-1, 0.9]);
-createPlanet(1821, 'io.png');
-createPlanet(58232 * 3.4, 'saturn.png', [-1, 1.2]);
-createPlanet(2574, 'titan.png');
-createPlanet(25362, 'uranus.png');
-createPlanet(15299, 'neptune.png');
-createPlanet(1188, 'pluto.png');
+    switch (body.englishName) {
+        case "Sun":
+            isSun = true;
+            skyMesh.visible = false;
+            groundPlane.visible = false;
 
+            scene.background = new THREE.Color('white');
+            break;
+        case "Mercury":
+            createPlanet(2439, 'mercury.png');
+            break;
+        case "Venus":
+            createPlanet(6051.8, 'venus.png');
+            break;
+        case "Earth":
+            createPlanet(6371, 'earth.png');
+            break;
+        case "Moon":
+            createPlanet(1737.4, 'moon.png');
+            break;
+        case "Mars":
+            createPlanet(3389.5, 'mars.png');
+            break;
+        case "Jupiter":
+            createPlanet(69911, 'jupiter.png', [-1, 0.9]);
+            break;
+        case "Saturn":
+            createPlanet(58232 * 3.4, 'saturn.png', [-1, 1.2]);
+            break;
+        case "Io":
+            createPlanet(1821, 'io.png');
+            break;
+        case "Uranus":
+            createPlanet(25362, 'uranus.png');
+            break;
+        case "Neptune":
+            createPlanet(15299, 'neptune.png');
+            break;
+        case "Pluto":
+            createPlanet(1188, 'pluto.png');
+            break;
+        case "Titan":
+            createPlanet(2574, 'titan.png');
+            break;
+    }
+
+    onWindowResize();
+};
 
 // Rendering
 let startTime = Date.now();
@@ -194,6 +248,7 @@ function animate() {
 	// renderer.render(scene, camera);
     composer.render();
 
+
     if (skyMesh) // Twinkle over time
         skyMesh.material.uniforms.rand1.value = timeElapsed / 20;
 }
@@ -204,9 +259,20 @@ animate();
 // On resize
 window.addEventListener('resize', onWindowResize, false);
 
+function getBase() {
+    let base = document.getElementById('myCanvas');
+    base = {
+        innerWidth: base.offsetWidth,
+        innerHeight: base.offsetHeight
+    };
+    return base;
+}
+
 function onWindowResize(){
-    camera.aspect = window.innerWidth / window.innerHeight;
+    let base = getBase();
+    camera.aspect = base.innerWidth / base.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    composer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setSize(base.innerWidth, base.innerHeight);
+    composer.setSize(base.innerWidth, base.innerHeight);
+    
 }
